b02cbafed171fada72ced3b082dae52e4a65e7b5
Mohammad s. Albay <43953576+MohammadAlbay@users.noreply.github.com>
2024-11-09 12:16:51 +0200

    Went Online

---
+++ lujain-webapp/resources/views/info.blade.php
@@ -0,0 +1,16 @@
+<!Doctype html>
+
+<html>
+    <head>
+        <title>Lujain GP Info<title> 
+        <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <h1>Lujain Graduation Project 🎓💻😉</h1>
+        <hr>
+        <h2>Change List:</h2>
+
+        <div>Just Published online</div>
+    </body>
+</html>
